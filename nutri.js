6faa842c554ad0367ca0b0c909f39a3327e0adb1
Surya Srikar Peruri <suryasrikar1997@gmail.com>
2018-08-01 23:31:55 +0530

    All filters and columns added

--- nutri.js
+++ nutri.js
@@ -2,39 +2,35 @@ google.charts.load("current", {packages:["corechart"]});
 
 var header = ["Timestamp", "Name", "Gender", "Age", "How often do you think about your health?", "Do you have any specific health goals?", "If yes, it would be great if you could let us know about them in brief.", "What makes you think about your health? Select all that apply.", "How do you envision a better version of yourself? Select all that apply.", "What are some common ailments that you suffer from? Please select all that apply.", "Do you have any nutritional deficiencies? ", "If yes, can you briefly mention what deficiencies you suffer from (For example: Vitamins (A,B,C,D,E,K), Iron, Calcium, Iodine etc.)", "Do you consume any of the following ?", "Do you take any nutritional supplements (For example: Multivitamins, Biotin, Mineral supplements, etc.)? ", "How did you come to know about these supplements?", "Do you take supplements through the year or intermittently?", "How do you take your supplements?", "Where do you purchase your supplements from?", "What is important for you when you purchase the supplements?" ,  "What types of supplements do you prefer?" , "On a scale of 1-10, how likely are you to undertake a brand switch from your current supplements?", "What do you feel is lacking in the existing supplements in the market?", "What are some lifestyle changes that you would want to make?", "Which factor(s) motivate/influence you to work on improving your health/appearance?", "How would you rate your health/appearance in comparison to people of your age group on a scale of 1 to 5? ", "Who are your role models, key opinion leaders when it comes to health and wellness ?", "Are you interested in consuming nutritional supplements in the future? ", "If you answered 1, 2 or 3 in the previous question, can you briefly state what makes you disinterested in consuming supplements in the future?", "If you answered 4 or 5, can you briefly state what interests you about nutritional supplements?", "Which format of nutritional supplement is attractive to you?", "", "", "", ""];
 
-var data = [[],[],["Male", "Female"], ["24", "25-29","30-34", "35-40", "40"], ["Very Often", "Sometimes", "Rarely", "Never"], ["Yes","No"], [], [] ,[], [], ["Yes","No"], [], ["Meat", "Dairy products", "None"], ["Yes", "No"], ["Doctor", "Online", "Referral", "Nutritionist"], ["Always", "Intermittently"], [], ["Offline Store", "Online Store", "Nutritionist/Doctor Samples"], ["Quality", "Price", "Brand", "Accessibility", "Doctor/Nutritionist Recommended"], ["Ayurvedic", "Non-Ayurvedic", "Whatever"], [1,2,3,4,5,6,7,8,9,10], [], [], [], [1,2,3,4,5], [], [1,2,3,4,5]];
+var data = Answers = [[],[],["Male", "Female"], ["<24", "25-29","30-34", "35-40", "40+"], ["Very Often", "Sometimes", "Rarely", "Never"], ["Yes","No"], [], ["While reading an article/journal on health","While watching Advertisements","Checking myself out in the mirror/selfie/photo","Waking up in the morning","While watching movies/series","While having a meal","Others"] ,["Better Focus, Attention, or Concentration","More Physically Active","Better Immunity","More Stamina","Better Weight Management","Better Stress Management","More Patience","More Self-confident","More Optimistic","Others"], ["Menstrual Cramps","Headache/Fever","Acidity/Indigestion/Constipation","Cold/Cough","Stress","Oral Problems","Muscular Ache","Hair Loss/thinning/damage","Vision Related Problems - Blurry vision, dry eyes, etc.","Skin Problems","Memory or Cognitive Functions","Sleep Related - Insomnia, Difficulty Falling Asleep, etc.","Low Energy","Others"], ["Yes","No"], [], ["Meat", "Dairy products", "None"], ["Yes", "No"], ["Doctor", "Online Research", "Referral", "Nutritionist", "Others"], ["Always", "Intermittently"], ["During/with meals","Before a meal","After a meal","No fixed routine","Others"], ["Offline Store", "Online Store", "Nutritionist's/Doctors Samples","Others"], ["Quality", "Price", "Brand", "Ingredients Sources", "Others"], ["Ayurvedic", "Non-Ayurvedic", "Whatever is available"], ["1","2","3","4","5","6","7","8","9","10"], ["Potency","Activation Time", "Transparency - ingredients and side-effects","Availability","Packaging","Others"], ["Reduce/Quit Smoking","Responsible Drinking","Manage Eating Habits","Exercise more or be more physically active","Maintain better personal hygiene","Get adequate sleep or maintain a better sleep cycle","Spend more time with friends and family","Be more productive at work","Focus more on appearance","Others"], ["Family","Peer Group","Work Place","Media/Advertisements","Others"], ["1","2","3","4","5"], [], ["1","2","3","4","5"],[],[],["Tablets/ Capsules","Powder/ Instant mixes","Gummy bears/ candies (Chewables)","Protein powder ","Drops ","Others"],[],[],[],[]];
 
-var numbers = [2,3,4,5,10,12,13,14,15,17,18,19,20,24,26];
-
+var numbers = [2,3,4,5,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,26,29];
+for (var i = 0; i < data.length; i++) {
+  console.log(data[i].length);
+}
 var filters = {};
 var visualizingcolumn = 0;
 
-function myFunction(id,num){
-  var checkbox = document.getElementById(id+num);
-  if(checkbox.checked == true){
-    if(id == 'dat'){
-      var i = String(num).slice(-1);
-      var j = String(num).slice(0, (String(num).length - 1));
-      filters[String(j)] = data[j][i];
+function chFunction(id,lis){
+  if(id == 'ch'){
+    var checkbox = document.getElementById(id+String(lis));
+    if(checkbox.checked == true){
+      visualizingcolumn = lis;
     }
     else{
-      visualizingcolumn = num;
+      if(visualizingcolumn==lis){ visualizingcolumn = 0 ; }
     }
+  }
+}
+
+function datFunction(id,num1,num2){
+  var checkbox = document.getElementById((id+num1) + num2);
+  if(checkbox.checked == true){
+      filters[String(num1)] = data[num1][num2];
   }
   else{
-    if(id == 'dat'){
-      var i = String(num).slice(-1);
-      var j = String(num).slice(0, (String(num).length -1));
-      filters.remove(j);
-    }
-    else{
-      if(visualizingcolumn==num){
-        visualizingcolumn = 0;
-      }
-    }
+    if(filters[String(num1)]==data[num1][num2]){ delete(filters[num1]); }
   }
-  console.log(filters);
-  console.log(visualizingcolumn);
 }
 
 function Showchart(){
@@ -42,15 +38,40 @@ function Showchart(){
     alert('Please select a column and some filters for visualizing!')
   }
   else{
-    $.get('http://35.154.42.243:5000/getfilterdatavisualize/' + JSON.stringify(filters) +"/"+ visualizingcolumn, function(data,status){
+    $.post('http://0.0.0.0:5000/getfilterdatavisualize_new',{"column":visualizingcolumn,"filters":JSON.stringify(filters)}, function(data,status){
       data = JSON.parse(data);
       console.log(data);
-      drawChart(data);
+      if(data['chart'] == 1){
+        drawpieChart(data['data']);
+      }
+      if(data['chart'] == 2){
+        drawbarChart(data['data']);
+      }
     });
   }
 }
 
-function drawChart(dat) {
+function drawbarChart(data) {
+  var bar_data = google.visualization.arrayToDataTable(data);
+  var view = new google.visualization.DataView(bar_data);
+  view.setColumns([0, 1,
+                   { calc: "stringify",
+                     sourceColumn: 1,
+                     type: "string",
+                     role: "annotation" },
+                   2]);
+  var options = {
+    title: "",
+    width: 800,
+    height: 800,
+    bar: {groupWidth: "80%"},
+    legend: { position: "none" },
+  };
+  var chart = new google.visualization.BarChart(document.getElementById("piechart_3d"));
+  chart.draw(view, options);
+}
+
+function drawpieChart(dat) {
   var data = google.visualization.arrayToDataTable(dat);
   var options = {
     title: '',
--- nutrition.py
+++ nutrition.py
@@ -1,5 +1,5 @@
 from gevent import monkey; monkey.patch_all()
-from bottle import route, run, response
+from bottle import route, run, response, request
 import bottle
 import csv
 import sys
@@ -18,111 +18,173 @@ class EnableCors(object):
                 return fn(*args, **kwargs)
         return _enable_cors
 
-global header
-global Jumpsouldata
+global Questions
+global Answers
+global JumpsoulSurvey
 
-header = []
-Jumpsouldata = []
+Questions = []
+JumpsoulSurvey = []
+Answers = [[],[],["Male", "Female"], ["<24", "25-29","30-34", "35-40", "40+"], ["Very Often", "Sometimes", "Rarely", "Never"], ["Yes","No"], [], ["While reading an article/journal on health","While watching Advertisements","Checking myself out in the mirror/selfie/photo","Waking up in the morning","While watching movies/series","While having a meal","Others"] ,["Better Focus, Attention, or Concentration","More Physically Active","Better Immunity","More Stamina","Better Weight Management","Better Stress Management","More Patience","More Self-confident","More Optimistic","Others"], ["Menstrual Cramps","Headache/Fever","Acidity/Indigestion/Constipation","Cold/Cough","Stress","Oral Problems","Muscular Ache","Hair Loss/thinning/damage","Vision Related Problems - Blurry vision, dry eyes, etc.","Skin Problems","Memory or Cognitive Functions","Sleep Related - Insomnia, Difficulty Falling Asleep, etc.","Low Energy","Others"], ["Yes","No"], [], ["Meat", "Dairy products", "None"], ["Yes", "No"], ["Doctor", "Online Research", "Referral", "Nutritionist", "Others"], ["Always", "Intermittently"], ["During/with meals","Before a meal","After a meal","No fixed routine","Others"], ["Offline Store", "Online Store", "Nutritionist's/Doctors Samples","Others"], ["Quality", "Price", "Brand", "Ingredients Sources", "Others"], ["Ayurvedic", "Non-Ayurvedic", "Whatever is available"], ["1","2","3","4","5","6","7","8","9","10"], ["Potency","Activation Time", "Transparency - ingredients and side-effects","Availability","Packaging","Others"], ["Reduce/Quit Smoking","Responsible Drinking","Manage Eating Habits","Exercise more or be more physically active","Maintain better personal hygiene","Get adequate sleep or maintain a better sleep cycle","Spend more time with friends and family","Be more productive at work","Focus more on appearance","Others"], ["Family","Peer Group","Work Place","Media/Advertisements","Others"], ["1","2","3","4","5"], [], ["1","2","3","4","5"],[],[],["Tablets/ Capsules","Powder/ Instant mixes","Gummy bears/ candies (Chewables)","Protein powder ","Drops ","Others"],[],[],[],[]]
+
+numbers = [2,3,4,5,7,8,9,10,12,13,14,15,16,17,18,19,20,21,22,23,24,26,29]
 
 app = bottle.app()
 
-def makeJumpsouldata():
-    global header
-    global Jumpsouldata
-    header = []
-    Jumpsouldata = []
+def readJumpsoulSurveydata():
+    global Questions
+    global JumpsoulSurvey
+    Questions = []
+    JumpsoulSurvey = []
     with open('JumpSoulSurvey-Combined.csv','r') as fil:
         reader = csv.reader(fil)
         i = 0
         for row in reader:
             if i == 0:
-                header = row
+                Questions = row
             else:
-                Jumpsouldata.append(row)
+                JumpsoulSurvey.append(row)
             i += 1
 
-@app.route('/getcolumns')
-def getcolumns():
-    global header
-    print(len(header))
-    yield json.dumps(header)
+@app.route('/getheaderdata')
+def getheaderdata():
+    global Questions
+    yield json.dumps(Questions)
 
-@app.route('/getfilterdatavisualize/<filters>/<column>')
-def filterdatavisualize(filters,column):
-    global header
-    global Jumpsouldata
-    filters = json.loads(filters)
-    data = []
-    for i in range(len(Jumpsouldata)):
-        c = 0
-        for key in filters:
-            val = header[int(key)]
-            if key == '3':
-                if filters[key] == '24':
-                    if int(Jumpsouldata[i][int(key)]) <= int(filters[key]):
-                        c += 1
-                elif filters[key] == '25-29':
-                    if int(Jumpsouldata[i][int(key)]) >= 25 and int(Jumpsouldata[i][int(key)]) <= 29:
-                        c += 1
-                elif filters[key] == '30-34':
-                    if int(Jumpsouldata[i][int(key)]) >= 30 and int(Jumpsouldata[i][int(key)]) <= 34:
-                        c += 1
-                elif filters[key] == '35-40':
-                    if int(Jumpsouldata[i][int(key)]) >= 35 and int(Jumpsouldata[i][int(key)]) <= 40:
-                        c += 1
-                elif filters[key] == '40':
-                    if int(Jumpsouldata[i][int(key)]) > 40:
-                        c += 1
-            elif key in ['12', '18', '19']:
-                sting = Jumpsouldata[i][int(key)].split(';')
-                for srt in sting:
-                    if srt.lower() in filters[key].lower():
-                        c += 1
-                        Jumpsouldata[i][int(key)] = srt
-            elif key in ['14', '17'] :
-                if filters[key].lower() in Jumpsouldata[i][int(key)].lower():
-                    c += 1
-                    Jumpsouldata[i][int(key)] = filters[key]
-            else:
-                if Jumpsouldata[i][int(key)] == filters[key]:
-                    c += 1
-        if c == len(filters.keys()) :
-            data.append(Jumpsouldata[i])
-    result = {}
-    for i in range(len(data)):
-        if data[i][int(column)] not in result:
-            result[data[i][int(column)]] = 1
-        else:
-            result[data[i][int(column)]] += 1
-    resultlist = [[header[int(column)], 'Count']]
-    for key in result:
-        resultlist.append([key, result[key]])
-    yield json.dumps(resultlist)
-
-@app.route('/getcolumndata/<columns>')
-def getcolumndata(columns):
-    global header
-    global Jumpsouldata
+@app.route('/getdistinctcolumndata/<columns>')
+def getdisticnctcolumndata(columns):
+    global Questions
+    global JumpsoulSurvey
     result = []
-    for i in range(len(Jumpsouldata)):
-        if Jumpsouldata[i][int(columns)] not in result:
-            result.append(Jumpsouldata[i][int(columns)])
+    for i in range(len(JumpsoulSurvey)):
+        if JumpsoulSurvey[i][int(columns)] not in result:
+            result.append(JumpsoulSurvey[i][int(columns)])
     yield json.dumps(result)
 
-@app.route('/multipledatacolumn/<column>')
-def getmultipledatacolumn(column):
-    global header
-    global Jumpsouldata
+@app.route('/getdistinctmultipledatacolumn/<column>')
+def getdistinctmultipledatacolumn(column):
+    global Questions
+    global JumpsoulSurvey
     result = []
-    for i in range(len(Jumpsouldata)):
-        sting = Jumpsouldata[i][int(column)]
-        sting = sting.split(';')
+    for i in range(len(JumpsoulSurvey)):
+        sting = (JumpsoulSurvey[i][int(column)]).split(';')
         for srt in sting:
             if srt not in result:
                 result.append(srt)
     yield json.dumps(result)
 
-makeJumpsouldata()
+@app.post('/getfilterdatavisualize_new')
+def getfilterdatavisualize_new():
+    global Questions
+    global JumpsoulSurvey
+    filters = json.loads(request.forms.get("filters"))
+    column = int(request.forms.get("column"))
+    data = []
+    for i in range(len(JumpsoulSurvey)):
+        c = 0
+        for key in filters:
+            if key in ['2','4','5','10','13','20','24','26','29']:
+                if JumpsoulSurvey[i][int(key)] == filters[key] :
+                    c += 1
+            if key == '3':
+                if filters[key] == '<24':
+                    if int(JumpsoulSurvey[i][int(key)]) <= 24:
+                        c += 1
+                elif filters[key] == '25-29':
+                    if int(JumpsoulSurvey[i][int(key)]) >= 25 and int(JumpsoulSurvey[i][int(key)]) <= 29:
+                        c += 1
+                elif filters[key] == '30-34':
+                    if int(JumpsoulSurvey[i][int(key)]) >= 30 and int(JumpsoulSurvey[i][int(key)]) <= 34:
+                        c += 1
+                elif filters[key] == '35-40':
+                    if int(JumpsoulSurvey[i][int(key)]) >= 35 and int(JumpsoulSurvey[i][int(key)]) <= 40:
+                        c += 1
+                elif filters[key] == '40+':
+                    if int(JumpsoulSurvey[i][int(key)]) > 40:
+                        c += 1
+            if key in ['7','8','9','12','14','15','16','17','18','19','21','22','23']:
+                if filters[key] != "Others" :
+                    for sting in (JumpsoulSurvey[i][int(key)]).split(';'):
+                        if sting == filters[key]:
+                            c +=1
+                            break
+                else:
+                    anwoth = [ans for ans in Answers[int(key)] if ans!='Others']
+                    for sting in (JumpsoulSurvey[i][int(key)]).split(';'):
+                        if sting not in anwoth:
+                            c += 1
+                            break
+        if c == len(filters.keys()):
+            data.append(JumpsoulSurvey[i][int(column)])
+    result = {}
+    answ = Answers[int(column)]
+    for ans in answ:
+        result[ans] = 0
+    if len(data) != 0 :
+        for i in range(len(data)):
+            if int(column) in [7,8,9,12,14,15,16,17,18,19,21,22,23]:
+                stdans = [ans for ans in answ if ans!='Others']
+                for dat in data[i].split(';'):
+                    if dat in stdans:
+                        result[dat] += 1
+                    else:
+                        if dat == "" or dat == "I AM A DOCTOR" or dat == "NO ISSUE":
+                            if 'NULL' in result.keys():
+                                result['NULL'] += 1
+                            else:
+                                result['NULL'] = 1
+                        else:
+                            if 'Others' in result.keys():
+                                result['Others'] += 1
+                            else:
+                                result['Others'] = 1
+            if int(column) in [2,4,5,10,13,20,24,26,29]:
+                if data[i] in [ans for ans in result.keys() if ans!='Others']:
+                    result[data[i]] += 1
+                elif data[i] == "":
+                    if 'NULL' in result.keys():
+                        result['NULL'] += 1
+                    else:
+                        result['NULL'] = 1
+                else:
+                    if 'Others' in result.keys():
+                        result['Others'] += 1
+                    else:
+                        result['Others'] = 1
+            if int(column) == 3:
+                if int(data[i]) <= 24:
+                    result['<24'] += 1
+                elif int(data[i]) >= 25 and int(data[i]) <= 29:
+                    result['25-29'] += 1
+                elif int(data[i]) >= 30 and int(data[i]) <= 34:
+                    result['30-34'] += 1
+                elif int(data[i]) >= 35 and int(data[i]) <= 40:
+                    result['35-40'] += 1
+                elif int(data[i]) > 40:
+                    result['40+'] += 1
+        percen = 0
+        for key in result:
+            result[key] = (result[key]*100)/len(data)
+            percen += result[key]
+        if percen == 100.0:
+            resultlist = [[Questions[int(column)],"Percent"]]
+            for key in result:
+                resultlist.append([key,result[key]])
+            yield json.dumps({"data":resultlist,"chart":1})
+        else:
+            colors = ["#b87333","silver","gold","green","red","blue","orange","black","#1A5276","#C70039","FB8C00","BF360C","#455A64","#827717","#33691E"]
+            resultlist = [[Questions[int(column)],"Percent",{'role':"style"}]]
+            i = 0
+            for key in result:
+                resultlist.append([key,result[key],colors[i]])
+                i += 1
+            yield json.dumps({"data":resultlist,"chart":2})
+    else:
+        resultlist = [[Questions[int(column)],"Percent"]]
+        for key in Answers[int(column)]:
+            resultlist.append([key,0])
+        yield json.dumps({"data":resultlist,"chart":1})
+
+readJumpsoulSurveydata()
 
 app.install(EnableCors())
 app.run(host='0.0.0.0', port=5000, debug=True, server='gevent')
